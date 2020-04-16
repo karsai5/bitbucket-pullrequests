882f9686aed492a22bf5f0ff4fac0431197f1e44
Linus Karsai <lkarsai@atlassian.com>
2020-04-16 14:24:39 +1000

    More refactoring code into seperate files

--- index.js
+++ index.js
@@ -1,51 +1,9 @@
-const axios = require("axios");
-const Table = require("cli-table3");
 const ora = require("ora");
 const auth = require("./lib/auth");
+const printTable = require("./lib/printTable");
+const api = require("./lib/api");
 
 const spinner = ora("Loading pull requests 💪");
-const instance = axios.create({
-  baseURL: "https://api.bitbucket.org/2.0/",
-});
-const getPullRequests = async (username, password) => {
-  const result = await instance.get(
-    "repositories/atlassian/atlassian-frontend/pullrequests",
-    {
-      params: {
-        q: 'author.nickname="zzarcon" AND state="OPEN"',
-      },
-      auth: {
-        username: username,
-        password: password,
-      },
-    }
-  );
-
-  return result.data.values;
-};
-
-const printTable = (pullRequests) => {
-  var table = new Table({
-    head: ["ID", "Title", "Author"],
-  });
-
-  const tableRows = pullRequests.map((pullRequest) => {
-    return [pullRequest.id, pullRequest.title, pullRequest.author.nickname];
-  });
-
-  table.push(...tableRows);
-
-  console.log(table.toString());
-};
-
-const logPullRequests = (pullRequests) => {
-  console.log(
-    pullRequests.map((item) => {
-      const { id, title, author, description } = item;
-      return { id, title, author, description };
-    })
-  );
-};
 
 const main = async () => {
   try {
@@ -53,7 +11,7 @@ const main = async () => {
     const password = auth.getPassword();
 
     spinner.start();
-    const pullRequests = await getPullRequests(username, password);
+    const pullRequests = await api.getPullRequests(username, password);
     spinner.stop();
 
     printTable(pullRequests);
