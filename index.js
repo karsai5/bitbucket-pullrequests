2a30007bda199b5e128143227ee82338eb7047f0
Linus Karsai <lkarsai@atlassian.com>
2020-04-16 13:49:58 +1000

    initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const axios = require("axios");
+var Table = require("cli-table3");
+
+const instance = axios.create({
+  baseURL: "https://api.bitbucket.org/2.0/",
+});
+
+const getPullRequests = async () => {
+  const result = await instance.get(
+    "repositories/atlassian/atlassian-frontend/pullrequests",
+    {
+      params: {
+        q: 'author.nickname="zzarcon" AND state="OPEN"',
+      },
+      auth: {
+        username: process.env.BITBUCKET_USERNAME,
+        password: process.env.BITBUCKET_PASSWORD
+      }
+    }
+  );
+
+  return result.data.values;
+};
+
+const printTable = (pullRequests) => {
+  var table = new Table({
+    head: ["ID", "Title", "Author"],
+  });
+
+  const tableRows = pullRequests.map((pullRequest) => {
+    return [pullRequest.id, pullRequest.title, pullRequest.author.nickname];
+  });
+
+  table.push(...tableRows);
+
+  console.log(table.toString());
+};
+
+const logPullRequests = (pullRequests) => {
+  console.log(
+    pullRequests.map((item) => {
+      const { id, title, author, description } = item;
+      return { id, title, author, description };
+    })
+  );
+};
+
+const main = async () => {
+  const pullRequests = await getPullRequests();
+  printTable(pullRequests);
+};
+
+main();
